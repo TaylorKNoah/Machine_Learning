01d19958177528f5d5222f593581b8b26154efea
Taylor Noah <tnoah@pdx.edu>
2021-12-16 11:17:13 -0800

    add to load_data in OR perceptron

--- Perceptrons/Logic_Gates/Or/Perceptron_OR_Gate.py
+++ Perceptrons/Logic_Gates/Or/Perceptron_OR_Gate.py
@@ -1,14 +1,23 @@
 
-
-def Perceptron_OR:
+def Perceptron_OR_Gate:
 
     def __init__(self):
+        self.Gamma = 0.1
+        self.Training_Data = []
+        self.Testing_Data = []
         return
 
-    def read_data(self, filename):
+    def load_data(self, TYPE, FILE):
+        if TYPE != "TESTING" and TYPE != "TRAINING":
+            print("Perceptron_OR_Gate.load_data usage(TYPE, FILE)")
+            print("Where:")
+            print("-TYPE must wither be \"TESTING\" or \"TRAINING\"")
+            print("-FILE must be a file location")
+            return
+
         data = []
 
-        with open(filename, "r") as file:
+        with open(FILE, "r") as file:
             for line in file:
                 line_data = line[:-1]
                 data.append(line_data)
