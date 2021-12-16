8990401ad32fddf0c211598a33170a6b105505e2
TaylorKNoah <73961491+TaylorKNoah@users.noreply.github.com>
2021-12-16 11:35:14 -0800

    finish load data. add init weights. add inputs, weights, and output to self via __init__.

--- Perceptrons/Logic_Gates/Or/Perceptron_OR_Gate.py
+++ Perceptrons/Logic_Gates/Or/Perceptron_OR_Gate.py
@@ -1,10 +1,16 @@
-
-def Perceptron_OR_Gate:
+from random import randint as rand
+class Perceptron_OR_Gate:
 
     def __init__(self):
         self.Gamma = 0.1
+
+        self.inputs = [1,0,0]
+        self.weights = [[0, 0, 0], [0, 0, 0]]
+        self.output = 0
+
         self.Training_Data = []
         self.Testing_Data = []
+
         return
 
     def load_data(self, TYPE, FILE):
@@ -16,11 +22,21 @@ def Perceptron_OR_Gate:
             return
 
         data = []
-
         with open(FILE, "r") as file:
             for line in file:
                 line_data = line[:-1]
                 data.append(line_data)
+
+        if TYPE == "TRIANING":
+            self.Training_Data = data.copy()
+        elif TYPE == "TESTING":
+            self.Testing_Data = data.copy()
+    
+    def init_weights(self):
+        for i in range(2):
+            for j in range(3):
+                r = rand(-50, 50) / 100
+                self.weights[i][j] = r
 
 
 
